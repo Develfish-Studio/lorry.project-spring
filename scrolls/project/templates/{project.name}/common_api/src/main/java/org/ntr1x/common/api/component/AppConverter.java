71105d00463a487947774c1299a1441311b75017
Igor Pavlenko <i.pavlenko@ntr1x.com>
2025-08-25 15:35:45 +0500

    Modules & Assemblies

--- scrolls/project/templates/{project.name}/common_api/src/main/java/org/ntr1x/common/api/component/AppConverter.java
+++
@@ -1,6 +0,0 @@
-package org.ntr1x.common.api.component;
-
-import org.springframework.core.convert.converter.Converter;
-
-public interface AppConverter<S, T> extends Converter<S, T> {
-}
--- scrolls/project/templates/{project.name}/common_web/src/main/java/org/ntr1x/common/web/component/AppArgumentResolver.java
+++
@@ -1,6 +0,0 @@
-package org.ntr1x.common.web.component;
-
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-
-public interface AppArgumentResolver extends HandlerMethodArgumentResolver {
-}
